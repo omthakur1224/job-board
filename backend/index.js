9a6f85ad03bc66b06e49c4df2d1a4372fe4adda5
omthakur1224 <omthakur1224@gmail.com>
2022-07-06 00:24:07 +0530

    hell

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express=require("express");
+const connect = require("./configs/server");
+const cors= require("cors")
+const app=express();
+app.use(cors())
+app.use(express.json());
+
+app.use('/',require('./routes/todoRoutes'))
+// app.use('/Todos',require('./routes/todoRoutes'))
+const PORT=9999;
+
+app.listen(PORT, async (req,res)=>{
+     await connect();
+             // .then((res)=>console.log("connection with mongo success")/)
+             console.log(`server is running on ${PORT}`)
+})
---
+++ backend/models/jobModel.js
@@ -0,0 +1,16 @@
+const mongoose= require('mongoose');
+
+const jobSchema = new mongoose.Schema({
+    "company_name":{type:String,required:true},
+      "job_title": {type:String,required:true},
+      
+      "Selection_Process": {type:String,required:true},
+    
+      "Qualification": {type:String,required:true},
+      "Required_Skills": {type:String,required:true},
+      "Experience": {type:String,required:true},
+      "Minimum_Salary": {type:String,required:true},
+      "Maximum_Salary": {type:String,required:true},
+     })
+
+module.exports= mongoose.model("Jobs",jobSchema);
